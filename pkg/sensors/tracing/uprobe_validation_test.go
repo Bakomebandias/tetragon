36475bbd626d094886be1f0b171a62119d731813
Federico Di Pierro <nierro92@gmail.com>
2026-05-27 15:29:08 +0200

    cleanup: moved kprobe and uprobe validation to k8s x-validation markers.
    The only field we can pre-validate are:
    * kprobe `Return -> requires `returnArg`
    * uprobe `Return -> requires `returnArg`
    * uprobe at most one between `addrs,symbols,offsets`
    * policy handler -> at most one between `kprobes;tracepoints;uprobes;lsmhooks;usdts;fentries`
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- pkg/sensors/tracing/uprobe_validation_test.go
+++ pkg/sensors/tracing/uprobe_validation_test.go
@@ -10,6 +10,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/cilium/tetragon/pkg/tracingpolicy"
+
 	"github.com/cilium/tetragon/pkg/testutils"
 )
 
@@ -40,5 +42,64 @@ spec:
 `
 
 	err := checkCrd(t, crd)
+	require.Error(t, err)
+}
+
+func testUprobeValidationSymbolsAddrsOffsets(t *testing.T, withSymbol, withAdrr, withOff bool) {
+	uprobe := testutils.RepoRootPath("contrib/tester-progs/usdt-override")
+	crd := `
+apiVersion: cilium.io/v1alpha1
+kind: TracingPolicy
+metadata:
+  name: "uprobe"
+spec:
+  uprobes:
+  - path: "` + uprobe + `"`
+	if withSymbol {
+		crd += "    symbols: [\"test_3\"]\n"
+	}
+	if withAdrr {
+		crd += "    addrs: [0x985256]\n"
+	}
+	if withOff {
+		crd += "    offsets: [0x9156366]\n"
+	}
+
+	_, err := tracingpolicy.FromYAML(crd)
+	require.Error(t, err)
+}
+
+func TestUprobeValidationSymbolsAddrsOffsets(t *testing.T) {
+	testUprobeValidationSymbolsAddrsOffsets(t, true, true, true)
+}
+
+func TestUprobeValidationSymbolsAddrs(t *testing.T) {
+	testUprobeValidationSymbolsAddrsOffsets(t, true, true, false)
+}
+
+func TestUprobeValidationSymbolsOffsets(t *testing.T) {
+	testUprobeValidationSymbolsAddrsOffsets(t, true, false, true)
+}
+
+func TestUprobeValidationAddrsOffsets(t *testing.T) {
+	testUprobeValidationSymbolsAddrsOffsets(t, false, true, true)
+}
+
+func TestUprobeValidationReturnWithoutArg(t *testing.T) {
+	uprobe := testutils.RepoRootPath("contrib/tester-progs/usdt-override")
+	crd := `
+apiVersion: cilium.io/v1alpha1
+kind: TracingPolicy
+metadata:
+  name: "uprobe"
+spec:
+  uprobes:
+  - path: "` + uprobe + `"
+    symbols:
+    - "test_3"
+    return: true
+`
+
+	_, err := tracingpolicy.FromYAML(crd)
 	require.Error(t, err)
 }
